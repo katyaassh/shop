91629c28784a57e48d154ee3e5f0342ac5f2aa7e
a.seltsov <a.seltsov@azoft.com>
2023-01-19 18:40:03 +0700

    сделала новую шапку

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,14 @@
-import React, {Suspense} from "react";
-import s from "./App.module.scss";
+import React from "react";
 import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import clsx from "clsx";
-
-
-const Profile = React.lazy(() => import('./pages/Profile/Profile').then((module) => ({
-    default: module.Profile
-})));
-
-const Feed = React.lazy(() => import('./pages/Feed/Feed').then((module) => ({
-    default: module.Feed
-})));
-
-const Messages = React.lazy(() => import('./pages/Messages/Messages').then((module) => ({
-    default: module.Messages
-})));
-
-const Friends = React.lazy(() => import('./pages/Friends/Friends').then((module) => ({
-    default: module.Friends
-})));
-
-const Groups = React.lazy(() => import('./pages/Groups/Groups').then((module) => ({
-    default: module.Groups
-})));
-
-const Photos = React.lazy(() => import('./pages/Photos/Photos').then((module) => ({
-    default: module.Photos
-})));
-
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className={s.app}>
+            <div>
                 <Header/>
-                <main className={clsx('container', s.main)}>
-                    <Navbar/>
-                    <div className={s.content}>
-                        <Suspense fallback={<div>loading...</div>}>
-                            <Routes>
-                                <Route path='/profile' element={<Profile/>}/>
-                                <Route path='/feed' element={<Feed/>}/>
-                                <Route path='/messages' element={<Messages/>}/>
-                                <Route path='/friends' element={<Friends/>}/>
-                                <Route path='/groups' element={<Groups/>}/>
-                                <Route path='/photos' element={<Photos/>}/>
-                            </Routes>
-                        </Suspense>
-                    </div>
+                <main>
+                    Тут все будет
                 </main>
             </div>
         </BrowserRouter>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,43 @@
-import logo from "../../assets/icons/logo.svg"
+import logo from "../../assets/image/logo.svg"
 import search from "../../assets/icons/search.svg"
-import avatar from "../../assets/image/avatar.jpg"
+import menu from "../../assets/icons/menu.svg"
+import user from "../../assets/icons/user.svg"
+import like from "../../assets/icons/like.svg"
+import cart from "../../assets/icons/cart.svg"
 import chevron from "../../assets/icons/chevron-down.svg"
 import s from "./Header.module.scss"
 import React from "react";
 import clsx from "clsx"
-import {NavLink} from "react-router-dom";
 
 interface IProps {
 
 }
 
 export const Header: React.FC<IProps> = () => {
-
     return <header className={s.headerContainer}>
         <div className={clsx('container', s.header)}>
             <div className={s.sectionLeft}>
-                <img src={logo} alt="ВКонтакте" className={s.logo}/>
-                <div className={s.search}>
-                    <img src={search} alt="Search" className={s.searchIcon}/>
-                    <span className={s.searchText}>Поиск</span>
+                <img src={menu} alt="Menu" className={s.menuIcon}/>
+                <div className={s.label}>Интернет магазин парфюмерии</div>
+                <div className={s.call}>
+                    <div className={s.numberBlock}>
+                        <span className={s.number}>+7 (383) 123 45 67</span>
+                        <img src={chevron} alt="Chevron" className={s.chevron}/>
+                    </div>
+                    <span className={s.callText}>ЗАКАЗАТЬ ЗВОНОК</span>
                 </div>
             </div>
-            <div className={s.sectionRight}>
-                <NavLink to={'/search'} className={s.searchButton}>
-                    <img src={search} alt="Search" className={s.searchIcon}/>
-                </NavLink>
-                <div className={s.user}>
-                    <img src={avatar} alt="User name" className={s.avatar}/>
-                    <img src={chevron} alt="Chevron" className={s.chevron}/>
+            <img src={logo} alt="Camilla De Luxe" className={s.logo}/>
+            <div className={s.headerActions}>
+                <img src={search} alt="Search" className={s.icon}/>
+                <img src={user} alt="User" className={s.icon}/>
+                <div className={clsx(s.headerAction, s.likeBlock)}>
+                    <img src={like} alt="Like" className={s.icon}/>
+                    <div className={s.count}>0</div>
+                </div>
+                <div className={s.headerAction}>
+                    <img src={cart} alt="Cart" className={s.icon}/>
+                    <div className={s.count}>0</div>
                 </div>
             </div>
         </div>
